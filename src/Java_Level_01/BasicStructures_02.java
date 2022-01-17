7f8b6327c189e7258ecead35a1163ec1b8fb7fef
Dniv2018 <NikolaevDmitry77@gmail.com>
2022-01-17 23:55:02 +0300

    Урок 2. Основные конструкции Метод проверки суммы 2-х чисел на нахождение в заданном пределе.

--- src/Java_Level_01/BasicStructures_02.java
+++ src/Java_Level_01/BasicStructures_02.java
@@ -1,4 +1,16 @@
 package Java_Level_01;
 
 public class BasicStructures_02 {
+    public static void main(String[] args) {
+        int firstNumber = 100;
+        int secondNumber = 10;
+        System.out.println("Результат проверки на нахождение суммы чисел " + firstNumber + " и " + secondNumber +
+                " в пределах от 10 до 20 (включительно): " + sumValueInsideLimits(firstNumber, secondNumber));
+
+
+    }
+
+    static boolean sumValueInsideLimits(int first, int second){
+        return 10 <= (first + second) &  (first + second) <= 20;
+    }
 }

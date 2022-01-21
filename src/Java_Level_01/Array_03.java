28256dcfeb8415b9d26e0c6cee912360f8babc4f
Dniv2018 <NikolaevDmitry77@gmail.com>
2022-01-21 11:19:35 +0300

    Сделано 3 задания из 8

--- src/Java_Level_01/Array_03.java
+++ src/Java_Level_01/Array_03.java
@@ -1,4 +1,60 @@
 package Java_Level_01;
 
+import java.util.Arrays;
+
 public class Array_03 {
+    public static void main(String[] args) {
+        int[] firstArray = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+        System.out.println("1. С помощью цикла и условия заменить 0 на 1, 1 на 0 в массиве:");
+        printOneArray(firstArray);
+        switchBetweenNullAndOne(firstArray);
+        System.out.println("Рузультат:");
+        printOneArray(firstArray);
+        System.out.println(" ");
+
+        int[] arrayHundred = new int[100];
+        System.out.println("2. С помощью цикла заполнить массив длиной 100 значениями 1 2 3 4 5 6 7 8 … 100");
+        printOneArray(arrayHundred);
+        fillingUpArray(arrayHundred);
+        System.out.println("Рузультат:");
+        printOneArray(arrayHundred);
+        System.out.println(" ");
+
+        int[] arrayMult = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        System.out.println("3. В массиве числа меньшие 6 умножить на 2;");
+        printOneArray(arrayMult);
+        ifLessSixMultiplyTwo(arrayMult);
+        System.out.println("Рузультат:");
+        printOneArray(arrayMult);
+        System.out.println(" ");
+
+    }
+
+    static void ifLessSixMultiplyTwo (int[] a){
+        for (int i =0; i< a.length; i++) {
+            if (a[i]<6){
+                a[i]=a[i]*2;
+            }
+        }
+    }
+
+    static void fillingUpArray (int[] a){
+        for (int i =0; i< a.length; i++) {
+                a[i]=i+1;
+        }
+    }
+
+    private static void printOneArray(int[] array) {
+        System.out.println(Arrays.toString(array));
+    }
+
+    static void switchBetweenNullAndOne (int[] a){
+        for (int i =0; i< a.length; i++) {
+            if (a[i]==0) {
+                a[i]=1;
+            } else {
+                a[i] =0;
+            }
+        }
+    }
 }

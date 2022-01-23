4aa52c650e39732fdd3180a5ce396cb574019e3a
Dniv2018 <NikolaevDmitry77@gmail.com>
2022-01-24 02:34:57 +0300

    Начали делать задание "Крестики-нолики в процедурном стиле".

--- src/Java_Level_01/TickTackToe.java
+++ src/Java_Level_01/TickTackToe.java
@@ -1,4 +1,5 @@
 package Java_Level_01;
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -8,11 +9,19 @@ final char DOT_EMPTY = '.';
 final char DOT_X = 'X';
 final char DOT_O = 'O';
 final char DOT_DANGER = 'd';
-final
-char[][] map;
+
 Scanner sc = new Scanner(System.in);
 Random rand = new Random();
     public static void main(String[] args) {
+        int[] map = new int [3];
+        int l=3;
+        // Сколько горизонтальных линий может пройти через точку
+        for (int i =0; i< map.length;i++){
+            if (i+l<= map.length){
+                for (int j=0;j<l;j++) map[i+j] ++;
+            }
+        }
+        System.out.println(Arrays.toString(map));
 
     }
 }

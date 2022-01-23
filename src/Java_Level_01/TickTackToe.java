cc223c8cacbc686646c3b5dca595dcd8ce6f910a
Dniv2018 <NikolaevDmitry77@gmail.com>
2022-01-23 22:29:05 +0300

    Начали делать задание "Крестики-нолики в процедурном стиле".

--- src/Java_Level_01/TickTackToe.java
+++ src/Java_Level_01/TickTackToe.java
@@ -3,6 +3,15 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class TickTackToe {
+final int SIZE = 3;
+final char DOT_EMPTY = '.';
+final char DOT_X = 'X';
+final char DOT_O = 'O';
+final char DOT_DANGER = 'd';
+final
+char[][] map;
+Scanner sc = new Scanner(System.in);
+Random rand = new Random();
     public static void main(String[] args) {
 
     }

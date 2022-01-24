4fcdb31989a116bb06cec71a29a98636645b045e
Dniv2018 <NikolaevDmitry77@gmail.com>
2022-01-24 05:51:30 +0300

    Начали делать задание "Крестики-нолики в процедурном стиле".

--- src/Java_Level_01/TickTackToe.java
+++ src/Java_Level_01/TickTackToe.java
@@ -4,24 +4,72 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class TickTackToe {
-final int SIZE = 3;
-final char DOT_EMPTY = '.';
-final char DOT_X = 'X';
-final char DOT_O = 'O';
-final char DOT_DANGER = 'd';
+final static int SIZE = 3;
+final static int LINE =3;
+final static char DOT_EMPTY = '.';
+final static char DOT_X = 'X';
+final static char DOT_O = 'O';
+final static char DOT_DANGER = 'd';
+final static char[][] mapGame = new char[SIZE][SIZE];
 
 Scanner sc = new Scanner(System.in);
 Random rand = new Random();
     public static void main(String[] args) {
-        int[] map = new int [3];
-        int l=3;
-        // Сколько горизонтальных линий может пройти через точку
-        for (int i =0; i< map.length;i++){
-            if (i+l<= map.length){
-                for (int j=0;j<l;j++) map[i+j] ++;
+        initMap();
+        printMap();
+        int[][] map = new int[SIZE][SIZE];
+
+        // Сколько горизонтальных, вертикальных и диагональных линий может пройти через точку
+        for (int vertPoint=0; vertPoint<SIZE; vertPoint++){
+            for (int horizontPoint =0; horizontPoint< SIZE; horizontPoint++){
+                if (horizontPoint + (LINE-1) < SIZE){
+                    for (int linePoint = 0; linePoint< LINE; linePoint++) {
+                        map[vertPoint][horizontPoint+linePoint] ++;//по горизонтали
+                    }
+                }
+                if (vertPoint + (LINE-1) < SIZE){
+                    for (int linePoint = 0; linePoint< LINE; linePoint++) {
+                        map[vertPoint+linePoint][horizontPoint] ++;//по вертикали
+                    }
+                }
+                if((vertPoint+(LINE-1) < SIZE) & (horizontPoint+(LINE-1) < SIZE)){
+                    for (int linePoint = 0; linePoint< LINE; linePoint++){
+                        map[vertPoint+linePoint][horizontPoint+linePoint]++;//обратный слэш
+                    }
+                }
+                if((vertPoint+(LINE-1) < SIZE) & (horizontPoint-(LINE-1) >= 0)){
+                    for (int linePoint = 0; linePoint< LINE; linePoint++){
+                        map[vertPoint+linePoint][horizontPoint-linePoint]++;//прямой слэш
+                    }
+                }
             }
         }
-        System.out.println(Arrays.toString(map));
+        for (int i=0; i<SIZE; i++) {
+            System.out.println(Arrays.toString(map[i]));
+        }
 
+//        System.out.println(Arrays.toString(map));
+
+    }
+    private static void initMap() {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                mapGame[i][j] = DOT_EMPTY;
+            }
+        }
+    }
+
+    private static void printMap() {
+        for (int i = 0; i <= SIZE; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        for (int i = 0; i < SIZE; i++) {
+            System.out.print((i + 1) + " ");
+            for (int j = 0; j < SIZE; j++) {
+                System.out.print(mapGame[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }

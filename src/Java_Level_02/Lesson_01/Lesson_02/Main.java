ea6720b0b7ca40a2d38adb088cc2fa363992a7ea
Dniv2018 <NikolaevDmitry77@gmail.com>
2022-04-03 22:30:24 +0300

    Урок Java2-2 Сделан метод с остановками.

--- src/Java_Level_02/Lesson_01/Lesson_02/Main.java
+++ src/Java_Level_02/Lesson_01/Lesson_02/Main.java
@@ -9,40 +9,85 @@ public class Main {
         Solid error = new Solid("Массив с ошибками", new String[][] {{"1","1","1","1"},{"1","З", null,"1"},{"1","I","1","1"},{"1","1","1","1"}});
         Solid right = new Solid("Правильный массив", new String[][] {{"1","1","1","1"},{"1","1","1","1"},{"1","1","1","1"},{"1","1","1","1"}});
 
-        MyArrays arrays = new MyArrays(right, error, notright3, notright2, notright1);
+        MyArrays arrays = new MyArrays(right, error, notright1, notright2, notright3);
 
-
-        int count;
 
         for (Solid a: arrays.getArray()) {
-            System.out.println(a.getName());
-            String[][] massif = a.getMass();
-
-            int len = massif.length;
-            if (len != 4) throw new MyArraySizeException
-                    ("Ощибка размера массива. Число строк: надо - 4, найдено - " + len);
-            for (int i=0; i<len;i++){
-                int innerLen = massif[i].length;
-                if (innerLen !=4) throw new MyArraySizeException
-                        ("Ощибка размера массива. Число столбцов в строке с индексом " + i + ": надо - 4, найдено - " + innerLen);
-            }
-
-            count = 0;
-            for (int i=0; i<4;i++){
-                for (int j=0; j<4;j++){
-                    try {
-                        count += Integer.parseInt(massif[i][j]);
-                    }catch (MyArrayDataException e){
-                        System.out.println("В ячейке с координатами: [" + i + "][" + j + "] лежат неверные данные");
-                    }
-                }
-            }
-//            System.out.println(massif.length);
-//            System.out.println(massif[0].length);
-//            System.out.println(massif[1].length);
-            System.out.println(a.getName() + " имеет сумму " + count);
-            System.out.println(" ");
+            sumArrayUnStoppAble(a);
         }
 
+        System.out.println("\nМетод с останавливающими исключениями");
+        for (Solid a: arrays.getArray()) {
+            sumArrayStop(a);
+        }
+
+
+    }
+
+    private static void sumArrayUnStoppAble(Solid a) {
+        int count;
+        System.out.println(a.getName());
+        String[][] massif = a.getMass();
+        int len = massif.length;
+        try {
+            if (len != 4) throw new MyArraySizeException("");
+        }catch (MyArraySizeException e){
+            System.out.println("Ощибка размера массива. Число строк: надо - 4, найдено - " + len);
+        }
+//        if (len != 4) throw new MyArraySizeException
+//                ("Ощибка размера массива. Число строк: надо - 4, найдено - " + len);
+
+        for (int i=0; i<len;i++){
+            int innerLen = massif[i].length;
+            try {
+                if (innerLen !=4) throw new MyArraySizeException("");
+            }catch (MyArraySizeException e){
+                System.out.println("Ощибка размера массива. Число столбцов в строке с индексом " + i + ": надо - 4, найдено - " + innerLen);
+            }
+//            if (innerLen !=4) throw new MyArraySizeException
+//                    ("Ощибка размера массива. Число столбцов в строке с индексом " + i + ": надо - 4, найдено - " + innerLen);
+        }
+
+        count = 0;
+        for (int i=0; i<len;i++){
+            for (int j=0; j<massif[i].length;j++){
+                try {
+                    count += Integer.parseInt(massif[i][j]);
+                }catch (NumberFormatException e){
+                    System.out.println("В ячейке с координатами: [" + i + "][" + j + "] лежат неверные данные");
+//                        throw new MyArrayDataException("В ячейке с координатами: [" + i + "][" + j + "] лежат неверные данные");
+                }
+            }
+        }
+        System.out.println(a.getName() + " имеет сумму " + count);
+        System.out.println(" ");
+    }
+
+    private static void sumArrayStop(Solid a) {
+        int count;
+        System.out.println(a.getName());
+        String[][] massif = a.getMass();
+        int len = massif.length;
+        if (len != 4) throw new MyArraySizeException
+                ("Ощибка размера массива. Число строк: надо - 4, найдено - " + len);
+
+        for (int i=0; i<len;i++){
+            int innerLen = massif[i].length;
+            if (innerLen !=4) throw new MyArraySizeException
+                    ("Ощибка размера массива. Число столбцов в строке с индексом " + i + ": надо - 4, найдено - " + innerLen);
+        }
+
+        count = 0;
+        for (int i=0; i<len;i++){
+            for (int j=0; j<massif[i].length;j++){
+                try {
+                    count += Integer.parseInt(massif[i][j]);
+                }catch (NumberFormatException e){
+                    throw new MyArrayDataException("В ячейке с координатами: [" + i + "][" + j + "] лежат неверные данные");
+                }
+            }
+        }
+        System.out.println(a.getName() + " имеет сумму " + count);
+        System.out.println(" ");
     }
 }
--- src/Java_Level_02/Lesson_01/Lesson_02/MyArrayDataException.java
+++ src/Java_Level_02/Lesson_01/Lesson_02/MyArrayDataException.java
@@ -2,7 +2,7 @@ package Java_Level_02.Lesson_01.Lesson_02;
 
 public class MyArrayDataException extends NumberFormatException{
 
-//    MyArrayDataException(String str){
-//        super(str);
-//    }
+    MyArrayDataException(String str){
+        super(str);
+    }
 }

b13379f59189ef4b775687fe47c707009fb38eb5
Dniv2018 <NikolaevDmitry77@gmail.com>
2022-04-11 00:21:39 +0300

    Начало

---
+++ src/Java_Level_02/Lesson_JFX/src/sample/Controller.java
@@ -0,0 +1,15 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+
+public class Controller {
+
+    @FXML
+    TextArea dialogTextArea;
+
+    public void btnSend(ActionEvent actionEvent){
+
+    }
+}

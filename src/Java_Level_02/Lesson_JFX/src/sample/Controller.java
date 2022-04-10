0d03565e138f6c22c78053e264f1678c6e35fbc3
Dniv2018 <NikolaevDmitry77@gmail.com>
2022-04-11 01:19:52 +0300

    Задание выполнено

--- src/Java_Level_02/Lesson_JFX/src/sample/Controller.java
+++ src/Java_Level_02/Lesson_JFX/src/sample/Controller.java
@@ -1,15 +1,33 @@
 package sample;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Controller {
 
     @FXML
     TextArea dialogTextArea;
 
-    public void btnSend(ActionEvent actionEvent){
+    @FXML
+    TextField inText;
 
+    @FXML
+    Button btnSend;
+
+    public void Send(){
+        if (!inText.getText().equals("")){
+            ZonedDateTime dateTime = ZonedDateTime.now();
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm");
+            dialogTextArea.appendText(dateTime.format(formatter) + "\n");
+            dialogTextArea.appendText(inText.getText() + "\n\n");
+            inText.clear();
+            inText.requestFocus();
+        }
+        inText.requestFocus();
     }
 }
